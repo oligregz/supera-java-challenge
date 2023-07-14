a9435fc6de40092508908c5b3dfa37662f7a3e11
oligregz <gregorysak90@gmail.com>
2023-07-14 11:11:14 -0300

    feat: accont model implemented

--- src/main/java/br/com/banco/domain/accountModel/AccountModel.java
+++ src/main/java/br/com/banco/domain/accountModel/AccountModel.java
@@ -1,5 +1,72 @@
 package br.com.banco.domain.accountModel;
 
-public class AccountModel {
+import br.com.banco.domain.clientModel.ClientModel;
+import java.io.Serializable;
+import java.util.UUID;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "account_tb")
+public class AccountModel implements Serializable {
   
+  
+  private static final long serialVersionUID = 1L;
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private UUID id;
+  
+  private Long number;
+  
+  private Double amount;
+  
+  private ClientModel holder;
+  
+  // private Transactions transactions;
+  
+  public AccountModel() {}
+  
+  public AccountModel(Long number, Double amount, ClientModel holder) {
+    super();
+    this.number = number;
+    this.amount = amount;
+    this.holder = holder;
+  }
+
+  public static long getSerialversionuid() {
+    return serialVersionUID;
+  }
+
+  public UUID getId() {
+    return id;
+  }
+
+  public Long getNumber() {
+    return number;
+  }
+
+  public void setNumber(Long number) {
+    this.number = number;
+  }
+
+  public Double getAmount() {
+    return amount;
+  }
+
+  public void setAmount(Double amount) {
+    this.amount = amount;
+  }
+
+  public ClientModel getHolder() {
+    return holder;
+  }
+
+  public void setHolder(ClientModel holder) {
+    this.holder = holder;
+  }
 }
